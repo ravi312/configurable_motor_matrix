eb8e810bba204c833284c024464e2c157267c6c1
ravi312 <ravisinha312@gmail.com>
2020-01-30 19:02:40 +0530

    Updated for 16 motor matrix

--- sketch_dec13a_motorMatrix/sketch_dec13a_motorMatrix.ino
+++ sketch_dec13a_motorMatrix/sketch_dec13a_motorMatrix.ino
@@ -3,7 +3,7 @@
 
   Turns motors on in sequence for specific seconds, then off for specific seconds, repeatedly.
 
-  modified 27 Jan 2020
+  modified 29 Jan 2020
   by Ravi Sinha ( github username: ravi312)
   written 13 Dec 2019
   by Ravi Sinha ( github username: ravi312)
@@ -12,12 +12,12 @@
   
 
 */
-int S_pattern[] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, A1, A2, A3, A4, A5};
-int X_pattern[] = { 6, 7, A2 ,A3, A4, A1, 8 , 5, A5, 12, 3, 4, A0, 11, 10, 3};
+int S_pattern[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+//int X_pattern[] = { 6, 7, A2 ,A3, A4, A1, 8 , 5, A5, 12, 3, 4, A0, 11, 10, 3};
 
 void setup() {
   
-  for (byte i = 0; i < 15; i++) {
+  for (byte i = 0; i < 16; i++) {
     pinMode(S_pattern[i], OUTPUT);
   }
   
@@ -26,7 +26,7 @@ void setup() {
 void loop() {
   //motor( motor IDcode, ON duration, delay) time in millisec
    //For S pattern code
-   for (byte i = 0; i < 15; i++) {
+   for (byte i = 0; i < 16; i++) {
     motor(S_pattern[i],100,200);
   }
               
